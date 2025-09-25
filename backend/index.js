8d2053892ae1efcbc92ee983f81fd21d092e0fe4
ShreedharG <shreedhar.goyal@gmail.com>
2025-09-26 00:34:36 +0530

    Deployement

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-const url = `https://realtime-code-editor-final.onrender.com`;
+const url = `https://brocode-nqwv.onrender.com`;
 const interval = 30000;
 
 function reloadWebsite() {
